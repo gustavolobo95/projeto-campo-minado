8b0b97e5e335c44f1d37f0bd6433974967e41070
Gustavo Silva Lobo <gustavo.lobo95@gmail.com>
2022-07-08 17:13:03 -0300

    Criação dos atributos e construtor da classe Tabuleiro

---
+++ src/br/com/cursocod3r/cm/modelo/Tabuleiro.java
@@ -0,0 +1,20 @@
+package br.com.cursocod3r.cm.modelo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Tabuleiro {
+
+	private int quantidadeDeLinhas;
+	private int quantidadeDeColunas;
+	private int quantidadeDeMinas;
+	
+	private final List<Campo> campos = new ArrayList<Campo>();
+
+	public Tabuleiro(int quantidadeDeLinhas, int quantidadeDeColunas, int quantidadeDeMinas) {
+		this.quantidadeDeLinhas = quantidadeDeLinhas;
+		this.quantidadeDeColunas = quantidadeDeColunas;
+		this.quantidadeDeMinas = quantidadeDeMinas;
+	}
+	
+}
